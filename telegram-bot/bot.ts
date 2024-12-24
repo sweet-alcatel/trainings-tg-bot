fd16fa3c19aca72bbcfe4738360584439753b321
dmitriy.sharapov <dmitriy.sharapov@myoffice.team>
2024-12-24 22:54:17 +0300

    Линтер для телеграмм-бота

--- telegram-bot/bot.ts
+++ telegram-bot/bot.ts
@@ -1,12 +1,12 @@
-import { Bot, type Context, session } from "grammy";
+import { Bot, type Context, session } from 'grammy';
 import dotenv from 'dotenv';
 import {
-    type ConversationFlavor,
-    conversations,
-    createConversation,
-} from "@grammyjs/conversations";
+  type ConversationFlavor,
+  conversations,
+  createConversation,
+} from '@grammyjs/conversations';
 import { greeting } from './commands/greeting';
-import { getPersons } from "./commands/getPersons";
+import { getPersons } from './commands/getPersons';
 
 dotenv.config();
 
@@ -15,22 +15,28 @@ type MyContext = Context & ConversationFlavor;
 export const bot = new Bot<MyContext>(`${process.env.bot_token}`);
 
 bot.use(session({ initial: () => ({}) }));
+
 bot.use(conversations());
+
+bot.command('cancel', async (ctx) => {
+  await ctx.conversation.exit();
+  await ctx.reply('Выход...');
+});
 
 bot.use(createConversation(greeting));
 
-bot.command("start", async (ctx) => await ctx.reply("Привет!"));
+bot.command('start', async (ctx) => await ctx.reply('Привет!'));
 
 bot.command('help', async (ctx) => {
-    await ctx.reply('На данный момент бот умеет выполнять команды /greeting и /getPersons')
-})
+  await ctx.reply(`
+    На данный момент бот умеет выполнять команды /greeting и /getPersons.
+    Если вы застряли во время диалога, прожмите команду "/cancel"`);
+});
 
 bot.command('greeting', async (ctx) => {
-    await ctx.conversation.enter('greeting')
-})
+  await ctx.conversation.enter('greeting');
+});
 
 bot.command('getPersons', getPersons);
 
 bot.start();
-
-
--- telegram-bot/commands/getPersons.ts
+++ telegram-bot/commands/getPersons.ts
@@ -1,19 +1,20 @@
-import { CommandContext, Context } from "grammy";
+/* eslint-disable @typescript-eslint/no-require-imports */
+import { CommandContext, Context } from 'grammy';
 
 const fetch = require('node-fetch');
 
 export const getPersons = async (ctx: CommandContext<Context>) => {
-    const response = await fetch(`${process.env.domain}/api/v1/user/`);
+  const response = await fetch(`${process.env.domain}/api/v1/user/`);
 
-    const data = await response.json();
+  const data = await response.json();
 
-    for await (const person of data) {
-        ctx.reply(`
+  for await (const person of data) {
+    ctx.reply(`
         Имя: ${person.name} 
         Рост: ${person.height}
         Вес: ${person.weight}
         Цели: ${person.goals}
         Травмы: ${person.injuries}
-        Комментарий: ${person.comment}`)
-    }
-}
+        Комментарий: ${person.comment}`);
+  }
+};
